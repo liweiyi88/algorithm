13b0433accf5bd790215f65a20ef70b96a0ef581
liweiyi88 <weiyi.li713@gmail.com>
2023-03-27 14:35:57 +1100

    merge sort bottom-up and add benchmark

--- sort/mergesort_test.go
+++ sort/mergesort_test.go
@@ -1,6 +1,10 @@
 package sort
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestMergeSort(t *testing.T) {
 	example := []string{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"}
@@ -12,5 +16,23 @@ func TestMergeSort(t *testing.T) {
 		if v != expected[i] {
 			t.Errorf("expected: %v, actual: %v", expected, example)
 		}
+	}
+}
+
+func BenchmarkMergeSortLen10(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < b.N; i++ {
+		sample := rand.Perm(10)
+		mergesort := MergeSort[int]{}
+		mergesort.Sort(sample)
+	}
+}
+
+func BenchmarkMergeSortLenMillion(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < b.N; i++ {
+		sample := rand.Perm(1000000)
+		mergesort := MergeSort[int]{}
+		mergesort.Sort(sample)
 	}
 }
